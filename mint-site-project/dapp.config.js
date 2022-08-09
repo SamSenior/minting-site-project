e04099eadea68ed8f07fc68403b17bf096521a5d
Sam Senior <samuelsenior@developersinstitute.co.nz>
2022-08-09 16:30:46 +1200

    added some content to the mint page

---
+++ mint-site-project/dapp.config.js
@@ -0,0 +1,10 @@
+const config = {
+  title: "Complacent Monkey Dapp",
+  description: "A practise run dapp",
+  contractAddress: "0x52afbd46ECf1444a73C03b9c296E25c558291F1B",
+  maxMintAmount: 10,
+  presaleMaxMintAmount: 3,
+  price: 0.01,
+};
+
+export { config };
