bd3d7be01e160d43a67334d4eed3c8409d4d5810
Sam Senior <samuelsenior@developersinstitute.co.nz>
2022-08-09 12:59:14 +1200

    added custom font

--- mint-site-project/pages/index.js
+++ mint-site-project/pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       </Head>
 
       <main className="bg-red-700">
-        <h1>
+        <h1 className="font-kaushan">
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
       </main>
